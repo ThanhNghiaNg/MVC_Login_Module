8e9d9cd3adfc7b4e7726ff948da15bed6b27874d
JTNghia <nghiant089@gmail.com>
2022-11-29 16:23:52 +0700

    first commit

---
+++ Login/Controllers/LoginController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Login.Models;
+
+namespace Login.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Authorize(User modelUser)
+        {
+            modelUser.LoginErrorMessage = "";
+            using (LoginEntities db = new LoginEntities())
+            {
+                var userDetails = db.Users.Where(x => x.Username == modelUser.Username && x.Password == modelUser.Password).FirstOrDefault();
+                if (userDetails != null)
+                {
+                    Session["userID"] = userDetails.UserID;
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    modelUser.LoginErrorMessage = "Wrong Username or Password";
+                    return View("Index", modelUser);
+                }
+            }
+        }
+        public ActionResult Logout()
+        {
+            Session["userID"] = null;
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
